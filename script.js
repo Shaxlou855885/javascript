38a6568cc4b666034e0b5387a94fac82fabb0117
Shaxlou855885 <shaxloubaydullaeva4@gmail.com>
2024-11-26 16:41:44 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const name = prompt('ismingizni kiriting');
+console.log('ism:', name); 
+
+const surname = prompt('familiyangizni kiriting');
+console.log('familiya:', surname);
+
+const age = prompt( 'yoshingizni kiriting');
+console.log('yosh:', age);
+
+const subject = prompt( 'yoqtirgan faningizni kiriting');
+console.log('fan:', subject);
+
+const city = prompt('yashaydigan tumaningizni kiriting');
+console.log('tumaningiz:', city);
